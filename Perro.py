4f1b3fad93572c0212bf3dcb660c03810a58558c
Adrian Gonzalez <a21gomaad@iesgrancapitan.org>
2023-05-22 13:49:53 +0200

    Refactorizacion 1 Perro - Adrián González Martínez

--- Perro.py
+++ Perro.py
@@ -3,10 +3,15 @@ Clase Perro.
 
 Autor: Jaime Rabasco Ronda.
 """
+
+
 class Perro:
+    def __init__(self):
+        self.guau = 'Guau'
 
     def ladrar(self):
-        print('Guau');
+        print(self.guau)
+
 
 p = Perro();
 p.ladrar();
